6d33cd37144212d5ed65c77ee2c7bd8efabecf08
James N <jnguye63@att.net>
2023-08-19 16:31:18 -0400

    Added the play one round function

--- index.js
+++ index.js
@@ -8,14 +8,33 @@ function getComputerChoice(choice){
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-console.log(getComputerChoice(computer_choice));
+
 /*
 -Create a function the plays a single round of Rock Paper Scissors
 -Function should take in two parameters (playerSelection, computerSelection)
 -Return a string that declares the winner of the round
 */
 
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice(computer_choice);
 
+console.log(playerSelection);
+console.log(computerSelection);
+
+
+function play_one_round(playerSelection, computerSelection){
+    if(playerSelection==="Rock" && computerSelection==="Scissor"){
+        return "You Won! Rock beats Scissor";
+    }
+    else if(playerSelection==="Rock" && computerSelection==="Paper"){
+        return "You Lose! Paper beats Rocks";
+    }
+    else{
+        return "Tie!";
+    }
+}
+
+console.log(play_one_round(playerSelection, computerSelection));
 
 /*
 -Create a new function called game()
